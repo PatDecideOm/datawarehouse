bf2337f0de7f8936a2f51e118eff24ee0023042c
Patrice DecideOm <pmazel@decideom.fr>
2024-03-12 08:06:59 +0100

    update models

--- models/facts/fact_orders.sql
+++ models/facts/fact_orders.sql
@@ -6,8 +6,8 @@ with fact_orders as (
                 customer.c_name,
                 orders.o_orderstatus,
                 sum(orders.o_totalprice) as o_totalprice
-    from        {{ source('staging','CUSTOMER') }} as customer,
-                {{ source('staging','ORDERS')}} as orders
+    from        {{ source('erp_datawarehouse','CUSTOMER') }} as customer,
+                {{ source('erp_datawarehouse','ORDERS')}} as orders
     where       customer.c_custkey = orders.o_custkey
     group by    customer.c_custkey,
                 customer.c_name,
